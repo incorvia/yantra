8a03d52c3990ff8c1922ebd40965e8fdc4f28c15
Kevin Incorvia <github@incorvia.email>
2025-04-12 14:38:30 -0500

    initial commit

---
+++ lib/yantra.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "yantra/version"
+
+module Yantra
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_yantra.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestYantra < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Yantra::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
